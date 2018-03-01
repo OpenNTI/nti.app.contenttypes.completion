2cddc8f8fe4df20faa2f19b5c77ffcc222aea53c
Josh Zuech <josh.zuech@nextthought.com>
2018-03-01 16:31:12 -0600

    Decorate required state on ICompletableItem objects

--- src/nti/app/contenttypes/completion/decorators.py
+++ src/nti/app/contenttypes/completion/decorators.py
@@ -11,6 +11,8 @@ from __future__ import absolute_import
 from zope import component
 from zope import interface
 
+from zope.cachedescriptors.property import Lazy
+
 from zope.location.interfaces import ILocation
 
 from nti.app.contenttypes.completion import COMPLETION_POLICY_VIEW_NAME
@@ -21,7 +23,10 @@ from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecora
 
 from nti.appserver.pyramid_authorization import has_permission
 
+from nti.contenttypes.completion.interfaces import ICompletableItem
 from nti.contenttypes.completion.interfaces import ICompletionContext
+from nti.contenttypes.completion.interfaces import ICompletableItemContainer
+from nti.contenttypes.completion.interfaces import ICompletableItemDefaultRequiredPolicy
 from nti.contenttypes.completion.interfaces import ICompletionContextCompletionPolicyContainer
 
 from nti.dataserver.authorization import ACT_CONTENT_EDIT
@@ -89,3 +94,43 @@ class _CompletionContextSettingsDecorator(AbstractAuthenticatedRequestAwareDecor
             link.__name__ = ''
             link.__parent__ = context
             _links.append(link)
+
+
+@component.adapter(ICompletableItem)
+@interface.implementer(IExternalMappingDecorator)
+class _CompletableItemDecorator(AbstractAuthenticatedRequestAwareDecorator):
+    """
+    Decorate the :class:`ICompletableItem` with requirement information. This
+    requires being able to adapt :class:`ICompletableItem` to the correct
+    :class:`ICompletionContext`.
+    """
+
+    def __init__(self, context, request):
+        super(_CompletableItemDecorator, self).__init__(context, request)
+        self.context = context
+
+    @Lazy
+    def completion_context(self):
+        return ICompletionContext(self.context, None)
+
+    def _predicate(self, unused_context, unused_result):
+        completion_policy = ICompletionContextCompletionPolicyContainer(self.completion_context,
+                                                                        None)
+        return self._is_authenticated \
+           and self.completion_context is not None \
+           and completion_policy is not None \
+           and completion_policy.context_policy is not None
+
+    def _do_decorate_external(self, context, result):
+        required_container = ICompletableItemContainer(self.completion_context)
+        default_policy = ICompletableItemDefaultRequiredPolicy(self.completion_context)
+        is_required = required_container.is_item_required(context)
+        is_not_required = required_container.is_item_optional(context)
+        default_state = not is_required and not is_not_required
+        if default_state:
+            item_mime_type = getattr(context, 'mime_type', '')
+            is_required = item_mime_type in default_policy.mime_types
+        result['CompletionRequired'] = is_required
+        # We're default or if we are not explicitly required/not-required
+        result['CompletionDefaultState'] = default_state
+
