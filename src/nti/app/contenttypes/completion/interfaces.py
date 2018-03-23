a4a74456fe8a311173d71b24cc9fd09cb607f5d2
chris.utz <chris.utz@nextthought.com>
2018-03-22 21:02:50 -0500

    Add a view to get a user's completed items.

--- src/nti/app/contenttypes/completion/interfaces.py
+++ src/nti/app/contenttypes/completion/interfaces.py
@@ -9,3 +9,30 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 # pylint: disable=inherit-non-class,expression-not-assigned
+
+from zope import interface
+
+from nti.schema.field import Object
+
+from nti.contenttypes.completion.interfaces import ICompletionContext
+
+from nti.dataserver.interfaces import IACLProvider
+from nti.dataserver.interfaces import IUser
+
+
+class ICompletedItemsContext(interface.Interface):
+
+    completion_context = Object(ICompletionContext,
+                                title=u"The completion context this progress is context is rooted",
+                                required=True)
+    
+    user = Object(IUser,
+                  title=u'The user we are scoped to',
+                  required=False)
+
+class ICompletionContextACLProvider(IACLProvider):
+    """
+    An ACL provider giving permissions beneath an ICompletionContext.
+    Typically adapted from (ICompletionContext, *)
+    """
+    pass
