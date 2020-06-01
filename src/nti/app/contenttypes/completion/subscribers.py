dce6fb9fc35d6d19cb34605ea27cebf690812826
Josh Zuech <josh.zuech@nextthought.com>
2020-06-01 15:30:19 -0500

    Only handle per item events if the context is not being deleted anyway

--- src/nti/app/contenttypes/completion/subscribers.py
+++ src/nti/app/contenttypes/completion/subscribers.py
@@ -23,6 +23,7 @@ from nti.contenttypes.completion.interfaces import ICompletedItemContainer
 from nti.contenttypes.completion.utils import get_indexed_completed_items
 
 from nti.coremetadata.interfaces import IUser
+from nti.coremetadata.interfaces import IMarkedForDeletion
 
 from nti.site.interfaces import IHostPolicyFolder
 
@@ -59,10 +60,17 @@ def _on_completable_item_deleted(item, unused_event=None):
     if queryInteraction() is not None:
         logger.info("Removing completed items data for %s", item)
         site = IHostPolicyFolder(item, None) or getSite()
+        # Don't we just need the completion contexts?
         items = get_indexed_completed_items(items=(item.ntiid,),
                                             sites=(site.__name__,))
         # get completed item containers
         contexts = {ICompletionContext(x, None) for x in items}
+        # We only need to individually remove items if their contexts
+        # are not also being deleted. Deleting the contexts will clean
+        # these containers anyway, much more efficiently.
+        # Note: the subscriber to clean up a context's completed items
+        # is not yet registered for all completion contexts (for some reason).
+        contexts = {x for x in contexts if not IMarkedForDeletion.providedBy(x)}
         containers = {ICompletedItemContainer(x, None) for x in contexts}
         containers.discard(None)
         # remove item data
